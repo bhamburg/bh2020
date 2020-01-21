a867533beabbc12c045f5eda7149aebb965231ff
Brian Hamburg <bhamburg@gmail.com>
2020-01-20 23:31:51 -0500

    customized logo area and footer

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,13 +3,9 @@ import React, { Component } from 'react';
 const Banner = React.forwardRef(({ siteTitle }, ref) => (
   <section ref={ref} id="banner">
     <div className="inner">
-      <div className="logo">
-        <span className="icon fa-envelope" />
-      </div>
-      <h2>{siteTitle}</h2>
+      <h2><a href="/" class="logo"><span>bh</span></a> {siteTitle}</h2>
       <p>
-        Based upon the free + fully responsive site template by {' '}
-        <a href="http://html5up.net">HTML5 UP</a>
+        Web Developer and Plectrum Banjoist in Philadelphia
       </p>
     </div>
   </section>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,12 +7,9 @@ import ContactInfo from './ContactInfo';
 const Footer = () => (
   <section id="footer">
     <div className="inner">
-      <h2 className="major">Get in touch</h2>
+      <h2 className="major">Hit me up</h2>
       <p>
-        Cras mattis ante fermentum, malesuada neque vitae, eleifend erat.
-        Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis egestas,
-        purus ipsum consequat orci, sit amet lobortis lorem lacus in tellus. Sed
-        ac elementum arcu. Quisque placerat auctor laoreet.
+        I am available for freelance web development and design projects as well as traditional jazz banjo engagements.
       </p>
       <ContactForm />
       <ContactInfo />
