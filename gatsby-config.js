834ca596ea04a2063935a45067c9c9c5c3ece05d
bjh015 <brian.hamburg@jefferson.edu>
2020-02-03 14:51:48 -0500

    fixed noreferrers and removed react-next

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     'gatsby-plugin-layout', 
-    'gatsby-plugin-react-next',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -6,16 +6,16 @@ const ContactInfo = () => (
       Philadelphia, PA, USA
     </li>
     <li className="fa-twitter">
-      <a href="https://twitter.com/burgbits" target="_blank">twitter.com/burgbits</a>
+      <a href="https://twitter.com/burgbits" rel="noopener noreferrer" target="_blank">twitter.com/burgbits</a>
     </li>
     <li className="fa-instagram">
-      <a href="https://www.instagram.com/burgbits" target="_blank">instagram.com/burgbits</a>
+      <a href="https://www.instagram.com/burgbits" rel="noopener noreferrer" target="_blank">instagram.com/burgbits</a>
     </li>
     <li className="fa-gitlab">
-      <a href="https://gitlab.com/bhamburg" target="_blank">gitlab.com/bhamburg</a>
+      <a href="https://gitlab.com/bhamburg" rel="noopener noreferrer" target="_blank">gitlab.com/bhamburg</a>
     </li>
     <li className="fa-github">
-      <a href="https://github.com/bhamburg" target="_blank">github.com/bhamburg</a>
+      <a href="https://github.com/bhamburg" rel="noopener noreferrer" target="_blank">github.com/bhamburg</a>
     </li>
     <li className="fa-envelope">
       <a href="mailto:bhamburg@gmail.com">bhamburg@gmail.com</a>
